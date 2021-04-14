3570d2496269cf3a1366df4fca81305742d02101
maximzubkov <zubkov.md@phystech.edu>
2021-04-14 16:03:35 +0300

    Update tests

--- tests/test_pb.py
+++ tests/test_pb.py
@@ -29,7 +29,7 @@ config2 = dict(
 
 
 @torch.no_grad()
-def _test(naive_config: dict, fft_config: dict):
+def _test_pt(naive_config: dict, fft_config: dict):
     v = torch.rand(batch_size, seq_len, num_heads, embed_dim)
     fft_pos_bias = PositionalBias(**fft_config)
     fft_pos_bias.eval()
@@ -47,12 +47,17 @@ def _test(naive_config: dict, fft_config: dict):
     assert torch.allclose(ppb_orig, ppb_fft, atol=1e-3), "PPB not equal"
 
 
+def _test_flax(naive_config: dict, fft_config: dict):
+    pass
+
+
 def test_pos_bias_full():
     config1["pos_bias_type"] = "naive"
     config2["pos_bias_type"] = "fft"
     config1["bias_base_type"] = "full"
     config2["bias_base_type"] = "full"
-    _test(config1, config2)
+    _test_pt(config1, config2)
+    _test_flax(config1, config2)
 
 
 def test_pos_bias_2d_full():
@@ -60,7 +65,8 @@ def test_pos_bias_2d_full():
     config2["pos_bias_type"] = "fft_2d"
     config1["bias_base_type"] = "full"
     config2["bias_base_type"] = "full"
-    _test(config1, config2)
+    _test_pt(config1, config2)
+    _test_flax(config1, config2)
 
 
 def test_pos_bias_sym():
@@ -68,7 +74,8 @@ def test_pos_bias_sym():
     config2["pos_bias_type"] = "fft"
     config1["bias_base_type"] = "symmetric"
     config2["bias_base_type"] = "symmetric"
-    _test(config1, config2)
+    _test_pt(config1, config2)
+    _test_flax(config1, config2)
 
 
 def test_pos_bias_2d_sym():
@@ -76,4 +83,5 @@ def test_pos_bias_2d_sym():
     config2["pos_bias_type"] = "fft_2d"
     config1["bias_base_type"] = "symmetric"
     config2["bias_base_type"] = "symmetric"
-    _test(config1, config2)
+    _test_pt(config1, config2)
+    _test_flax(config1, config2)
